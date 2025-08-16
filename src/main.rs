1095d6287f6f524c61f930791d552548fc7f248a
sarunas.gincas <sarunikss@gmail.com>
2025-08-17 01:49:12 +0300

    feat: add amplify to the stack

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
+use aws_cdk_lib::{aws_amplify::{self, CfnAppProps}, aws_s3};
+
 mod helpers;
 
-use aws_cdk_lib::{aws_s3::Bucket, App, Stack};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let app = App::new(None);
+    let app = aws_cdk_lib::App::new(None);
 
-    let stack = Stack::new(Some(&app), Some("MyDataProjectStack".to_string()), None);
+    let stack = aws_cdk_lib::Stack::new(Some(&app), Some("MyDataProjectStack".to_string()), None);
 
-    let _bucket = Bucket::new(&stack, "MyDataBucket".to_string(), None);
+    let _bucket = aws_s3::Bucket::new(&stack, "MyDataProjectBucket".to_string(), None);
+
+    let _amplify = aws_amplify::CfnApp::new(&stack, "MyDataProjectAmplifyInstance".to_string(), CfnAppProps {
+        ..Default::default()
+    });
 
     helpers::synth_app(app)?;
 
