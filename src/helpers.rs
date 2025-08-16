d7b7f7e2c89c6246277b292399783b9ddcd9456f
sarunas.gincas <sarunikss@gmail.com>
2025-08-17 00:20:35 +0300

    fix: fix getters

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,18 +1,18 @@
-use aws_cdk_lib::App;
+use aws_cdk_lib::{App, StageTrait};
 
 pub fn synth_app(app: App) -> Result<(), Box<dyn std::error::Error>> {
     let cloud_assembly = app.synth(None);
 
-    let stacks = cloud_assembly.stacks();
+    let stacks = cloud_assembly.get_stacks();
 
     for stack in stacks {
-        let template_json = stack.template_json();
+        let template_json = stack.get_template();
         
         let json_pretty = serde_json::to_string_pretty(&template_json)?;
 
         std::fs::create_dir("cdk.out")?;
 
-        let filename = format!("cdk.out/{}-template.json", stack.stack_name());
+        let filename = format!("cdk.out/{}-template.json", stack.get_stack_name());
 
         std::fs::write(&filename, json_pretty)?;
     }
