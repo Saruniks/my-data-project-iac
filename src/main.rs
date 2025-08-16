88b536574243a5d4225823d898b7af5684a5b4da
sarunas.gincas <sarunikss@gmail.com>
2025-08-17 01:09:51 +0300

    feat: add stack to the app

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod helpers;
 
-use aws_cdk_lib::App;
+use aws_cdk_lib::{App, Stack};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app = App::new(None);
+
+    let _stack = Stack::new(Some(&app), Some("MyDataProjectStack".to_string()), None);
 
     helpers::synth_app(app)?;
 
