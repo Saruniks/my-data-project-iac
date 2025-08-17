f3357a212d41691ecba1e8044b754a1a34ca76c4
sarunas.gincas <sarunikss@gmail.com>
2025-08-18 01:52:46 +0300

    feat: add a cf distribution config placeholder

--- src/my_full_stack_stack/reverse_proxy.rs
+++ src/my_full_stack_stack/reverse_proxy.rs
@@ -4,6 +4,7 @@ pub struct ReverseProxy;
 impl ReverseProxy {
     pub fn new(stack: &aws_cdk_lib::Stack) -> Self {
         let _cloudfront = aws_cloudfront::CfnDistribution::new(stack, "MyDataProjectCloudFront".to_string(), aws_cloudfront::CfnDistributionProps {
+            distribution_config: serde_json::json!({}),
             ..Default::default()
         });
 
