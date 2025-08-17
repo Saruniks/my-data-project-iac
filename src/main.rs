715171fce2218cecb701d3e0d2754b4234b3221d
sarunas.gincas <sarunikss@gmail.com>
2025-08-17 17:26:55 +0300

    feat: add minimal cloudfront distribution to the stack

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use aws_cdk_lib::{aws_amplify, aws_s3, aws_elasticbeanstalk, /* aws_cloudfront */};
+use aws_cdk_lib::{aws_amplify, aws_s3, aws_elasticbeanstalk, aws_cloudfront};
 
 mod helpers;
 
@@ -10,9 +10,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let _bucket = aws_s3::Bucket::new(&stack, "MyDataProjectBucket".to_string(), None);
 
-    /* let _cloudfront = aws_cloudfront::CfnDistribution::new(&stack, "MyDataProjectCloudFront".to_string(), aws_cloudfront::CfnDistributionProps {
+    let _cloudfront = aws_cloudfront::CfnDistribution::new(&stack, "MyDataProjectCloudFront".to_string(), aws_cloudfront::CfnDistributionProps {
         ..Default::default()
-    }); */
+    });
 
     let _amplify = aws_amplify::CfnApp::new(&stack, "MyDataProjectAmplifyInstance".to_string(), aws_amplify::CfnAppProps {
         ..Default::default()
