30620d2fa3675fe7b5f3c3e8ae439155203d4d29
sarunas.gincas <sarunikss@gmail.com>
2025-08-28 14:32:55 +0300

    feat: copy cloudformation templates from cloud assembly dir instead of recreating json template files

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,23 +1,41 @@
-use aws_cdk_lib::{App, StageTrait};
+use std::ops::Deref;
+
+use aws_cdk_lib::{cx_api::CloudAssemblyTrait, App, StageTrait};
 
 pub fn synth_app(app: App) -> Result<(), Box<dyn std::error::Error>> {
     let cloud_assembly = app.synth(None);
 
     let stacks = cloud_assembly.get_stacks();
 
-    for stack in stacks {
-        let template_json = stack.get_template();
-        
-        let json_pretty = serde_json::to_string_pretty(&template_json)?;
+    // TODO: Is there a way to avoid ambiguity in cases like these while generating Rust code?
+    let assembly_dir = CloudAssemblyTrait::get_directory(cloud_assembly.deref());
+    println!("Cloud assembly directory: {}", assembly_dir);
 
-        if !std::path::Path::new("cdk.out").exists() {
-            std::fs::create_dir("cdk.out")?;
-        }
-
-        let filename = format!("cdk.out/{}.json", stack.get_stack_name());
-
-        std::fs::write(&filename, json_pretty)?;
+    let cdk_out = std::path::Path::new("cdk.out");
+    if !cdk_out.exists() {
+        std::fs::create_dir("cdk.out")?;
     }
 
+    copy_directory_contents(&assembly_dir, cdk_out)?;
+
+    Ok(())
+}
+
+fn copy_directory_contents(src: &str, dest: &std::path::Path) -> Result<(), Box<dyn std::error::Error>> {
+    for entry in std::fs::read_dir(src)? {
+        let entry = entry?;
+        let file_type = entry.file_type()?;
+        let src_path = entry.path();
+        let dest_path = dest.join(entry.file_name());
+
+        if file_type.is_dir() {
+            if !dest_path.exists() {
+                std::fs::create_dir(&dest_path)?;
+            }
+            copy_directory_contents(src_path.to_str().unwrap(), &dest_path)?;
+        } else {
+            std::fs::copy(&src_path, &dest_path)?;
+        }
+    }
     Ok(())
 }
